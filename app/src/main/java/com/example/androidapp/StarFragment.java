c99a5d938e6ee194567d58284bcd016487501db7
unexpectResult <1270901509@qq.com>
2020-12-27 17:50:16 +0800

    fragment

--- app/src/main/java/com/example/androidapp/StarFragment.java
+++ app/src/main/java/com/example/androidapp/StarFragment.java
@@ -28,33 +28,9 @@ public class StarFragment extends Fragment {
     LinearLayout pointLayout;
     private List<StarInfoBean.StarinfoBean> mDadas;
 
-    public StarFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StarFragment newInstance(String param1, String param2) {
-        StarFragment fragment = new StarFragment();
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_star, container, false);
         initView(view);
         Bundle bundle = getArguments();
@@ -71,6 +47,6 @@ public class StarFragment extends Fragment {
     private void initView(View view) {
         starVp = view.findViewById(R.id.starfrag_vp);
         starGv = view.findViewById(R.id.starfrag_gv);
-//        pointLayout = view.findViewById(R.id.starfrag_Layout);
+        pointLayout = view.findViewById(R.id.starfrag_layout);
     }
 }
