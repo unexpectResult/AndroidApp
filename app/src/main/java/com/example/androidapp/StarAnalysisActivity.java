5d5503f1e76d8d072a5100a76e2f6eadca68113d
unexpectResult <1270901509@qq.com>
2020-12-27 19:21:08 +0800

    2020/12/27 19:00

--- app/src/main/java/com/example/androidapp/StarAnalysisActivity.java
+++ app/src/main/java/com/example/androidapp/StarAnalysisActivity.java
@@ -3,12 +3,43 @@ package com.example.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class StarAnalysisActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StarAnalysisActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView titleTv;
+    ImageView backIv;
+    CircleImageView iconIv;
+    TextView nameTv,dateTv;
+    ListView analysisLv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_analysis);
+        initView();
+    }
+
+    private void initView() {
+        titleTv = findViewById(R.id.title_tv);
+        backIv = findViewById(R.id.title_iv_back);
+        iconIv = findViewById(R.id.staranalysis_iv);
+        nameTv = findViewById(R.id.staranalysis_tv_name);
+        dateTv = findViewById(R.id.staranalysis_tv_date);
+        analysisLv = findViewById(R.id.staranalysis_lv);
+
+        titleTv.setText("星座详细信息");
+        backIv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
